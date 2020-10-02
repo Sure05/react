a4ff4b028f1fcd233c54e3d9fb17c5008b9e374a
Sure05 <deurlex@gmail.com>
2020-10-02 15:36:19 +0300

    start hw2

--- src/App.js
+++ src/App.js
@@ -1,26 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Fragment, useState} from 'react';
+import GlideComponent from "./components/GlideComponent";
+
+import Form from "semantic-ui-react/dist/commonjs/collections/Form";
+import Radio from "semantic-ui-react/dist/commonjs/addons/Radio";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [type, setType] = useState('slider');
+    return (
+        <Fragment>
+            <Form>
+                <Form.Field>
+                    <Radio
+                        label='Type slider'
+                        name='radioGroup'
+                        value='slider'
+                        checked={type === 'slider'}
+                        onChange={() => setType('slider')}
+                    />
+                </Form.Field>
+                <Form.Field>
+                    <Radio
+                        label='Type carousel'
+                        name='radioGroup'
+                        value='carousel'
+                        checked={type === 'carousel'}
+                        onChange={() => setType('carousel')}
+                    />
+                </Form.Field>
+            </Form>
+            <GlideComponent type={type} />
+        </Fragment>
+    );
 }
 
 export default App;
